3c35b4ddc0d94c589a99de9c8293e134ec661a73
Alok Singhal <aloksinghal328@gmail.com>
2026-06-18 20:23:20 +0530

    fix: Change default DB host and configuration to 127.0.0.1 to avoid IPv6 loopback errors

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,7 +9,7 @@ declare global {
 export const pool =
   global.dbPool ??
   mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "127.0.0.1",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
